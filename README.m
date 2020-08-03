85a8da340a1bbe49054c5b4e92fc69af104423a6
nicaberry <44035759+nicaberry@users.noreply.github.com>
2020-08-03 09:35:55 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,2 +1,2 @@
-"# learnGrid" 
-![image](https://user-images.githubusercontent.com/44035759/89152913-93bc5900-d56c-11ea-998f-a6c1bdd9f83d.png)
+
+![image](https://user-images.githubusercontent.com/44035759/89152913-93bc5900-d56c-11ea-998f-a6c1bdd9f83d.png);
